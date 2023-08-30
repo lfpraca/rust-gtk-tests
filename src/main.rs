906c47f685f363fe97d6eb893c2d352cbe279207
lfpraca <luisfelippe@protonmail.com>
2023-08-30 17:30:53 -0300

    Made it more like a configuration manager

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
-use std::cell::Cell;
-use std::rc::Rc;
-
 use gtk::glib::clone;
-use gtk::prelude::*;
-use gtk::{glib, Application, ApplicationWindow, Button, Label, Orientation};
+use gtk::{prelude::*, Button};
+use gtk::{glib, Application, ApplicationWindow, Label, Orientation, Entry, PasswordEntry, Frame};
 
 const APP_ID: &str = "org.gtk_rs.HelloWorld2";
 
@@ -19,56 +16,70 @@ fn main() -> glib::ExitCode {
 }
 
 fn build_ui(app: &Application) {
-    // Create a button with label and margins
-    let button_increase = Button::builder()
-        .label("Increase")
+    let label_usuario = Label::builder()
+        .label("Usuário:")
         .margin_top(12)
         .margin_bottom(12)
         .margin_start(12)
         .margin_end(12)
         .build();
 
-    let button_decrease = Button::builder()
-        .label("Decrease")
+    let entry_usuario = Entry::builder().build();
+
+    let frame_entry_usuario = Frame::builder()
         .margin_top(12)
         .margin_bottom(12)
-        .margin_start(12)
+        .margin_end(12)
+        .tooltip_text("E-mail do usuário")
+        .child(&entry_usuario)
+        .build();
+
+    let box_usuario = gtk::Box::builder()
+        .orientation(Orientation::Horizontal)
+        .build();
+    box_usuario.append(&label_usuario);
+    box_usuario.append(&frame_entry_usuario);
+
+    let label_senha = Label::builder()
+        .label("Senha:")
+        .margin_bottom(12)
+        .margin_start(20)
         .margin_end(12)
         .build();
 
-    let number_label = Label::builder().label("Number: 0").build();
+    let entry_senha = PasswordEntry::builder()
+        .show_peek_icon(true)
+        .build();
 
-    let number = Rc::new(Cell::new(0));
+    let frame_entry_senha = Frame::builder()
+        .margin_bottom(12)
+        .margin_end(12)
+        .child(&entry_senha)
+        .build();
 
-    // button.connect_clicked(move |button| {
-    //     number.set(number.get() + 1);
-    //     button.set_label(&format!("Increase {}", number.get()));
-    // });
+    let box_senha = gtk::Box::builder()
+        .orientation(Orientation::Horizontal)
+        .visible(false)
+        .build();
+    box_senha.append(&label_senha);
+    box_senha.append(&frame_entry_senha);
 
-    button_increase.connect_clicked(clone!(@weak number, @weak number_label => move |_| {
-    number.set(number.get() + 1);
-    number_label.set_label(&format!("Number: {}", number.get()));
-    println!("DEBUG: number increased");
-    }));
-
-    button_decrease.connect_clicked(clone!(@weak number_label => move |_| {
-    number.set(number.get() - 1);
-    number_label.set_label(&format!("Number: {}", number.get()));
-    println!("DEBUG: number decreased");
-    }));
-
-    let my_box = gtk::Box::builder()
+    let ui_box = gtk::Box::builder()
         .orientation(Orientation::Vertical)
         .build();
-    my_box.append(&number_label);
-    my_box.append(&button_increase);
-    my_box.append(&button_decrease);
+    ui_box.append(&box_usuario);
+    ui_box.append(&box_senha);
+
+    entry_usuario.connect_changed(clone!(@weak box_senha => move |_|{
+        box_senha.set_visible(true);
+        println!("DEBUG: usuário alterado");
+    }));
 
     // Create a window and set the title
     let window = ApplicationWindow::builder()
         .application(app)
-        .title("Plugin Configuration Manager")
-        .child(&my_box)
+        .title("Configurações Plugin")
+        .child(&ui_box)
         .build();
 
     // window.set_child(Some(&button));
